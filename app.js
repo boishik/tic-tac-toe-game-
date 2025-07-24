17f47accf1136c0454922d48239513fc39be27ff
boishik <baruaboishik@gmail.com>
2025-07-24 23:07:48 +0600

    Tic-tac-toe game practice

---
+++ app.js
@@ -0,0 +1,79 @@
+let boxs = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true; //player turn
+
+const resetGame = () => {
+  turnO = true;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+};
+
+let winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+boxs.forEach((box) => {
+  box.addEventListener("click", () => {
+    console.log("box was clicked");
+    if (turnO) {
+      box.innerText = "O";
+      box.classList.add("o-style");
+      turnO = false;
+    } else {
+      box.innerText = "X";
+      box.classList.add("x-style");
+      turnO = true;
+    }
+
+    box.disabled = true;
+    checkWinner();
+  });
+});
+
+const disableBoxes = () => {
+  for (let box of boxs) {
+    box.disabled = true;
+  }
+};
+
+const enableBoxes = () => {
+  for (let box of boxs) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  for (patterns of winPatterns) {
+    let pos1Val = boxs[patterns[0]].innerText;
+    let pos2Val = boxs[patterns[1]].innerText;
+    let pos3Val = boxs[patterns[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val == pos2Val && pos2Val == pos3Val) {
+        console.log("Winner", pos1Val);
+        showWinner(pos1Val);
+      }
+    }
+  }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
